bab12af3c614fa5c1019b76428a3b08b72342167
snowdot <co.pascu@gmail.com>
2020-09-01 14:15:41 +0300

    add components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const element = <h1>Hello from Create Reac App</h1>
+// component file
+import TodoContainer from './components/TodoContainer'
+
+// const element = <h1>Hello from Create Reac App</h1>
 
 ReactDOM.render(
-    element,
+    <TodoContainer />,
     document.getElementById('root')
 )
