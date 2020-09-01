80d877370f1586a91318a45f8aa1b06e08e59cbb
snowdot <co.pascu@gmail.com>
2020-09-01 15:27:03 +0300

    add checkbox

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,14 @@ import React from 'react';
 // Class-Based Component
 class TodoItem extends React.Component {
     render() {
-        return <li>{this.props.todo.title}</li>;
+        return <li>
+            <input 
+            type="checkbox"
+            checked={this.props.todo.completed} 
+            onChange={() => console.log('click')}
+            />
+            {this.props.todo.title}
+        </li>;
     }
 }
 
