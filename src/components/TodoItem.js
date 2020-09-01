2c9d80349693038a5adf8f3926fb95b4389895ed
snowdot <co.pascu@gmail.com>
2020-09-01 14:33:55 +0300

    add components

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+// Class-Based Component
 class TodoItem extends React.Component {
     render() {
         return <li>{this.props.todo.title}</li>;
     }
 }
+
+// Function Component
+// function TodoItem(props) {
+//     return <li>{props.todo.title}</li>;
+// }
+
 
 export default TodoItem;
