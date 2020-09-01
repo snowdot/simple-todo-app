a32145f8201a2d583613650ce360950719993802
snowdot <co.pascu@gmail.com>
2020-09-01 16:04:43 +0300

    add event handler on checkboxes

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -22,11 +22,21 @@ class TodoContainer extends React.Component {
             }
         ]
     };
+    handleChange = id => {
+        this.setState({
+            todo: this.state.todos.map(todo => {
+                if(todo.id === id) {
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            })
+        });
+    };
     render() {
         return (
             <div>
                 <Header />
-                <TodoList todos={this.state.todos} />
+                <TodoList todos={this.state.todos} handleChangeProps={this.handleChange} />
             </div>
         );
     }
