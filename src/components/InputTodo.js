09459b68c7b61e258681fbf4aec612548873f2c3
snowdot <co.pascu@gmail.com>
2020-09-03 15:37:05 +0300

    computer switch

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,35 +1,73 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class InputTodo extends Component {
-    state = {
+const InputTodo = props => {
+    const [inputText, setInputText] = useState({
         title: ''
-    };
-    onChange = e => {
-        this.setState({
+    });
+
+    const onChange = e => {
+        setInputText({
+            ...inputText,
             [e.target.name]: e.target.value
         });
-    };
-    handleSubmit = e => {
+    }
+
+    const handleSubmit = e => {
         e.preventDefault();
-        this.props.addTodoProps(this.state.title);
-        this.setState({
+        props.addTodoProps(inputText.title);
+        setInputText({
             title: ''
         });
-    };
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit} className="form-container">
-                <input 
-                    type="text"
-                    name="title"
-                    placeholder="add todo..."
-                    value={this.state.title}
-                    onChange={this.onChange} className="input-text"
-                />
-                <input type="submit" value="Submit" className="input-submit" />
-            </form>
-        )
     }
+
+    return (
+        <form onSubmit={handleSubmit} className="form-container">
+            <input 
+                type="text"
+                name="title"
+                placeholder="add todo..."
+                value={inputText.title}
+                onChange={onChange} className="input-text"
+            />
+            <input type="submit" value="Submit" className="input-submit" />
+        </form>
+    )
 }
 
 export default InputTodo;
+
+// import React, { Component } from 'react';
+
+// class InputTodo extends Component {
+//     state = {
+//         title: ''
+//     };
+//     onChange = e => {
+//         this.setState({
+//             [e.target.name]: e.target.value
+//         });
+//     };
+//     handleSubmit = e => {
+//         e.preventDefault();
+//         this.props.addTodoProps(this.state.title);
+//         this.setState({
+//             title: ''
+//         });
+//     };
+//     render() {
+//         return (
+//             <form onSubmit={this.handleSubmit} className="form-container">
+//                 <input 
+//                     type="text"
+//                     name="title"
+//                     placeholder="add todo..."
+//                     value={this.state.title}
+//                     onChange={this.onChange} className="input-text"
+//                 />
+//                 <input type="submit" value="Submit" className="input-submit" />
+//             </form>
+//         )
+//     }
+// }
+
+// export default InputTodo;
--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,67 +1,124 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './TodosList';
 import Header from './Header';
 import InputTodo from './InputTodo';
-import axios from "axios";
 
-class TodoContainer extends React.Component {
-    state = {
-        todos: [],
-        show: false
-    };
-    handleChange = id => {
-        this.setState({
-            todos: this.state.todos.map(todo => {
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+
+const TodoContainer = props => {
+    const [todos, setTodos] = useState([]);
+    const [show, setShow] = useState(false);
+
+    const handleChange = id => {
+        setTodos(
+            todos.map(todo => {
                 if(todo.id === id) {
                     todo.completed = !todo.completed;
                 }
                 return todo;
-            }),
-            show: !this.state.show
-        });
-    };
-    delTodo = id => {
-        axios
-            .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-            .then(response =>
-                this.setState({
-                    todos: [
-                        ...this.state.todos.filter(todo => {
-                            return todo.id !== id;
-                        })
-                    ]
-                })
-            )
-    };
-    addTodoItem = title => {
-        axios
-            .post('https://jsonplaceholder.typicode.com/todos', {
-                title: title,
-                completed: false
             })
-            .then(response =>
-                this.setState({
-                    todos: [...this.state.todos, response.data]
-                })
-            )
-    };
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
-        .then(response => this.setState({ todos: response.data }));
-    };
-    render() {
-        return (
-            <div className="container">
-                <Header headerSpan={this.state.show}/>
-                <InputTodo addTodoProps={this.addTodoItem} />
-                <TodoList
-                    todos={this.state.todos}
-                    handleChangeProps={this.handleChange}
-                    deleteTodoProps={this.delTodo}
-                />
-            </div>
         );
+        setShow(!show);
     }
+
+    const delTodo = id => {
+        setTodos([
+            ...todos.filter(todo => {
+                return todo.id !== id;
+            }),
+        ]);
+    }
+
+    const addTodoItem = title => {
+        const newTodo = {
+            id: uuidv4(),
+            title: title,
+            completed: false,
+        }
+
+        setTodos([...todos, newTodo]);
+    }
+
+    return (
+        <div className="container">
+            <Header headerSpan={show}/>
+            <InputTodo addTodoProps={addTodoItem} />
+            <TodoList
+                todos={todos}
+                handleChangeProps={handleChange}
+                deleteTodoProps={delTodo}
+            />
+        </div>
+    )
 }
 
 export default TodoContainer;
+
+// import React from 'react';
+// import TodoList from './TodosList';
+// import Header from './Header';
+// import InputTodo from './InputTodo';
+// import axios from 'axios';
+
+// class TodoContainer extends React.Component {
+//     state = {
+//         todos: [],
+//         show: false
+//     };
+//     handleChange = id => {
+//         this.setState({
+//             todos: this.state.todos.map(todo => {
+//                 if(todo.id === id) {
+//                     todo.completed = !todo.completed;
+//                 }
+//                 return todo;
+//             }),
+//             show: !this.state.show
+//         });
+//     };
+//     delTodo = id => {
+//         axios
+//             .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+//             .then(response =>
+//                 this.setState({
+//                     todos: [
+//                         ...this.state.todos.filter(todo => {
+//                             return todo.id !== id;
+//                         })
+//                     ]
+//                 })
+//             )
+//     };
+//     addTodoItem = title => {
+//         axios
+//             .post('https://jsonplaceholder.typicode.com/todos', {
+//                 title: title,
+//                 completed: false
+//             })
+//             .then(response =>
+//                 this.setState({
+//                     todos: [...this.state.todos, response.data]
+//                 })
+//             )
+//     };
+//     componentDidMount() {
+//         axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+//         .then(response => this.setState({ todos: response.data }));
+//     };
+//     render() {
+//         return (
+//             <div className="container">
+//                 <Header headerSpan={this.state.show}/>
+//                 <InputTodo addTodoProps={this.addTodoItem} />
+//                 <TodoList
+//                     todos={this.state.todos}
+//                     handleChangeProps={this.handleChange}
+//                     deleteTodoProps={this.delTodo}
+//                 />
+//             </div>
+//         );
+//     }
+// }
+
+// export default TodoContainer;
